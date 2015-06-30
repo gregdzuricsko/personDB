18078fe963c64ae5c0efa59ffbfa7489253aea0e
Justin Rassier <justin.rassier@infinitecampus.com>
2015-06-30 11:39:07 -0500

    forgot my Address model

---
+++ src/js/models/Address.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var Address = function() {
+  this.address1 = "";
+  this.address2 = "";
+  this.city = "";
+  this.state = "";
+  this.zip = "";
+};
+
+
+module.exports = Address;
