3b7b5040a4766b4a0cc829d07e92b68fa314ecf5
Justin Rassier <justin.rassier@infinitecampus.com>
2015-06-30 11:25:45 -0500

    Add mutliple address form elements by clicking button

--- src/js/components/AddressForm.js
+++ src/js/components/AddressForm.js
@@ -2,16 +2,26 @@
 var React = require('react');
 
 var AddressForm = React.createClass({
+  getInitialState: function() {
+    return {
+      address: this.props.address
+    };
+  },
   render: function() {
     return (
       <div>
-        <input placeholder="Address 1" type="text"/>
-        <input placeholder="Address 2" type="text"/>
-        <input placeholder="City" type="text"/>
-        <input placeholder="State" type="text"/>
-        <input placeholder="Zip" type="text"/>
+        <input onChange={this.handleInputChange.bind(this, 'address1')} placeholder="Address 1" type="text" value={this.state.address.address1}/>
+        <input onChange={this.handleInputChange.bind(this, 'address2')} placeholder="Address 2" type="text" value={this.state.address.address2}/>
+        <input onChange={this.handleInputChange.bind(this, 'city')} placeholder="City" type="text" value={this.state.address.city}/>
+        <input onChange={this.handleInputChange.bind(this, 'state')} placeholder="State" type="text" value={this.state.address.state}/>
+        <input onChange={this.handleInputChange.bind(this, 'zip')} placeholder="Zip" type="text" value={this.state.address.zip}/>
       </div>
     );
+  },
+  handleInputChange: function(field, e) {
+    var newAddress = this.state.address;
+    newAddress[field] = e.target.value;
+    this.setState({address: newAddress});
   }
 
 });
--- src/js/components/PersonForm.js
+++ src/js/components/PersonForm.js
@@ -4,24 +4,32 @@ var React = require('react');
 var Person = require('../models/Person');
 var PersonActions = require('../actions/PersonActions');
 var AddressForm = require('./AddressForm');
+var Address = require('../models/Address');
 
 var PersonForm = React.createClass({
   getInitialState: function() {
     return {
       firstName: "",
       lastName: "",
-      buttonDisabled: true
+      buttonDisabled: true,
+      addresses: []
     };
   },
   componentDidMount: function() {
     React.findDOMNode(this.refs.firstName).focus();
   },
   render: function() {
+
+    var addressForms = this.state.addresses.map(function(address){
+      return <AddressForm address={address}/>;
+    });
+
     return (
       <form onSubmit={this.savePerson}>
         <input onChange={this.handleInputChange.bind(this, 'firstName')} placeholder="First Name" ref="firstName" type="text" value={this.state.firstName}/>
         <input onChange={this.handleInputChange.bind(this, 'lastName')} placeholder="Last Name" type="text" value={this.state.lastName}/>
-        <AddressForm/>
+        <button onClick={this.addAddressForm}>Add Address</button>
+        {addressForms}
         <input disabled={this.state.buttonDisabled} type="submit" value="Save Person"/>
       </form>
     );
@@ -33,11 +41,16 @@ var PersonForm = React.createClass({
     newState.buttonDisabled = (!newState.firstName && !newState.lastName);
     this.setState(newState);
   },
+  addAddressForm: function(e){
+    e.preventDefault();
+    var newState = this.state;
+    newState.addresses.push(new Address());
+    this.setState(newState);
+  },
   savePerson: function(e) {
-//prevent the postback of the form
     e.preventDefault();
 
-//fill out our person and call up to the main app to save
+    //fill out our person and call up to the main app to save
     var p = new Person();
     p.firstName = this.state.firstName.trim();
     p.lastName = this.state.lastName.trim();
@@ -45,7 +58,8 @@ var PersonForm = React.createClass({
       PersonActions.create(p);
       this.setState({
         firstName: "",
-        lastName: ""
+        lastName: "",
+        addresses: []
       });
       React.findDOMNode(this.refs.firstName).focus();
     }
