0eb4b5577dfff6746e23ec9741affffcaca4b1d3
Justin Rassier <justinrassier@outlook.com>
2015-07-06 20:49:18 -0500

    better configuration and setup of server plus static content serving

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,8 +1,8 @@
 "use strict";
 var mongoose = require('mongoose');
 
-module.exports = function() {
-  mongoose.connect("mongodb://justinrassier:Oeve6Ne5sG2p@ds053160.mongolab.com:53160/persondb");
+module.exports = function(config) {
+  mongoose.connect(config.db);
 
   var Person = mongoose.model('Person', {
     firstName: String,
